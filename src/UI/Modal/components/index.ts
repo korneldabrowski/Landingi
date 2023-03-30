553af9130350cf97a8dd26628c2efa96586db94d
Kornel Dabrowski <korneldabrowski@gmail.com>
2023-03-30 17:34:24 +0200

    feat: add Modal UI component

---
+++ src/UI/Modal/components/index.ts
@@ -0,0 +1,4 @@
+export { default as ModalBody } from './ModalBody';
+export { default as ModalTitle } from './ModalTitle';
+export { default as ModalFooter } from './ModalFooter';
+export { default as ModalButton } from './ModalButton';
---
+++ src/UI/Modal/variants.ts
@@ -0,0 +1,46 @@
+import { ColorMap } from './Modal.props';
+
+export const colorVariants: ColorMap = {
+  primary: {
+    title: 'text-primary',
+    overlay: 'bg-primary bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-primary',
+  },
+  secondary: {
+    title: 'text-secondary',
+    overlay: 'bg-secondary bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-secondary',
+  },
+  success: {
+    title: 'text-success',
+    overlay: 'bg-success bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-success',
+  },
+  warning: {
+    title: 'text-warning',
+    overlay: 'bg-warning bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-warning',
+  },
+  info: {
+    title: 'text-info',
+    overlay: 'bg-info bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-info',
+  },
+  error: {
+    title: 'text-error',
+    overlay: 'bg-error bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-error',
+  },
+  accent: {
+    title: 'text-accent',
+    overlay: 'bg-accent bg-opacity-25',
+    backdrop: 'backdrop-blur-sm',
+    button: 'btn-accent',
+  },
+};
